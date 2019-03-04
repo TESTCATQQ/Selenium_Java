1cebb955daf3b9797318c96b824a13cd998c154a
TESTCATQQ <36889056 + TESTCATQQ @ users.noreply.github.com>
2019-03-05 00:29:28 +0800

    findElenmts.extra and xuanting

--- Selenium_2019/src/Se_2019/findElement.java
+++ Selenium_2019/src/Se_2019/findElement.java
@@ -5,10 +5,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class findElement {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "G:\\myGit\\Selenium_Java\\my_env\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://www.baidu.com");
+		//driver.manage().window().maximize();//最大化
 		//driver.findElement(By.id("kw")).sendKeys("testId");//By.id
 		//driver.findElement(By.name("wd")).sendKeys("name");//By.name
 		//driver.findElement(By.className("s_ipt")).sendKeys("classname");//by.classname
@@ -16,6 +17,29 @@ public class findElement {
 		//driver.findElement(By.partialLinkText("新")).click();//与linktext类似，但只需要包含，即可访问
 		//System.out.println(driver.findElements(By.tagName("input")).size());//by tagname
 		//driver.findElement(By.xpath("//*[@id=\"kw\"]")).sendKeys("Xpath");//by Xpath
-		driver.findElement(By.cssSelector("#kw")).sendKeys("cssSelector");//cssSelector
+		//driver.findElement(By.cssSelector("#kw")).sendKeys("cssSelector");//cssSelector
+		//System.out.println(driver.findElement(By.id("kw")).getAttribute("class"));
+		//System.out.println(driver.findElement(By.id("kw")).getAttribute("maxlength"));
+		
+		/*driver.findElement(By.id("kw")).sendKeys("test1");
+		Thread.sleep(1000);
+		driver.findElement(By.id("kw")).clear();
+		Thread.sleep(1000);
+		driver.findElement(By.id("kw")).sendKeys("test2");
+		Thread.sleep(1000);*/
+		
+		//System.out.println(driver.findElement(By.id("kw")).isDisplayed());
+		//System.out.println(driver.findElement(By.linkText("新闻")).getText());
+		//System.out.println(driver.findElement(By.id("kw")).isSelected());
+		/*
+		driver.findElement(By.id("kw")).sendKeys("王琳同学");
+		//driver.findElement(By.id("kw")).submit();
+		driver.findElement(By.id("su")).click();*/
+		
+		//System.out.println(driver.getWindowHandle());//获取打开浏览器字符串？
+		driver.findElement(By.id("kw")).sendKeys("王琳同学");
+		driver.findElement(By.id("su")).click();
+		System.out.println(driver.getCurrentUrl());
+		
 	}
 }
---
+++ Selenium_2019/src/Se_2019/xuanting.java
@@ -0,0 +1,23 @@
+package Se_2019;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class xuanting {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "G:\\myGit\\Selenium_Java\\my_env\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.baidu.com");
+		//driver.findElement(By.id("kw")).sendKeys("Õı¡’");
+		//driver.findElement(By.id("su")).click();
+		//System.out.println(driver.getCurrentUrl());
+		
+		Actions action = new Actions(driver);
+		//action.clickAndHold(driver.findElement(By.className("pf"))).perform();
+		action.clickAndHold(driver.findElement(By.linkText("…Ë÷√"))).perform();
+		Thread.sleep(5000);
+		driver.findElement(By.className("setpref")).click();
+	}
+}
