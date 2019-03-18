398c387d2d57405d0c01c56a287a6c5c05c0c0a0
TESTCATQQ <36889056 + TESTCATQQ @ users.noreply.github.com>
2019-03-19 00:15:14 +0800

    testng parameter

--- Selenium_2019/src/Se_2019/test_ng_01.java
+++ Selenium_2019/src/Se_2019/test_ng_01.java
@@ -2,6 +2,7 @@ package Se_2019;
 
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.testng.Assert;
 
@@ -13,13 +14,17 @@ public class test_ng_01 {
 	}
 	
 	@Test(groups= {"smoke"})
-	public void test_01() {
-		Assert.assertEquals("2", "3");
+	@Parameters({"test"})
+	public void test_01(String test) {
+		System.out.println(test);
+		Assert.assertEquals(test, "3");
 	}
 	
 	@Test(groups= {"smoke"})
-	public void test_02() {
-		Assert.assertEquals("2", "2" ,"2.equals");
+	@Parameters({"test0319","test"})
+	public void test_02(String test0319,String test) {
+		System.out.println(test0319);
+		Assert.assertEquals(test0319, "Jun" ,"2.equals");
 	}
 	
 	@AfterTest
